f0fa9bdc2661c6c85b9584b4472a4e19f47cbab9
name <s111213223@stu.ntue.edu.tw>
2024-07-15 12:44:46 +0800

    python

--- index.py
+++ index.py
@@ -1,1 +1,11 @@
-print('heelo')
+import numpy as np 
+import matplotlib.pyplot as plt
+from  ipywidgets import interact
+
+n = 18
+x_values = np.linspace(-2, 3, n)
+y_values = x_values**2 - 1
+plt.plot(x_values, y_values, 'r')
+plt.legend([f"$y=x^2$ with {n} points"])
+plt.show()
+
